97fab06913e800446066bd13097f70340b74e96a
Faniriniaina7 <faniryrandriamparany@gmail.com>
2024-02-23 19:19:28 +0300

    modification refresh service

--- src/app/demo/components/pages/crud/service/service.component.ts
+++ src/app/demo/components/pages/crud/service/service.component.ts
@@ -8,7 +8,7 @@ import { PageEvent } from 'src/app/demo/interfaces/pageEvent';
 import { CategorieService } from 'src/app/demo/service/categorie/categorie.service';
 import { Categorie } from 'src/app/demo/interfaces/categorie';
 import { FormBuilder, FormGroup, NgForm,  Validators  } from '@angular/forms';
-import { BehaviorSubject, Observable,  distinctUntilChanged, of,  switchMap } from 'rxjs';
+import { BehaviorSubject, Observable,  Subject,  distinctUntilChanged, of,  switchMap } from 'rxjs';
 import { Statut } from 'src/app/demo/interfaces/statut';
 import { ServiceSearch } from 'src/app/demo/interfaces/serviceSearch';
 import { CustomResponse } from 'src/app/demo/interfaces/customResponse';
@@ -113,6 +113,8 @@ export class ServiceComponent implements OnDestroy,OnInit {
     get categorie() {
         return this.serviceForm.controls['categorie'];
     }
+
+    private serviceSubject= new Subject<Service[]>();
 
     constructor(
         private messageService: MessageService,
@@ -257,6 +259,9 @@ export class ServiceComponent implements OnDestroy,OnInit {
               if(response.status === 200) {
                   this.service = response.data;
                   //this.refreshServices$.next(null);
+                
+                this.services.push(this.unService);
+                this.serviceSubject.next(this.services);
                   this.messageService.add({ severity: 'success', summary: 'Success', detail: response.message, life: 3000 });
               }
             },
@@ -282,6 +287,8 @@ export class ServiceComponent implements OnDestroy,OnInit {
                 if(response.status == 201) {
                     this.serviceForm.reset();
                     this.serviceDialog = false;
+                this.services.push(this.unService);
+                this.serviceSubject.next(this.services);
                     this.messageService.add({ severity: 'success', summary: 'Success', detail: response.message, life: 3000 });
                 }
             },
@@ -313,6 +320,8 @@ export class ServiceComponent implements OnDestroy,OnInit {
                 this.unService.categorie = this.updateCategorie;
                 if(response.status == 200) {
                     this.ficheServiceDialog = false;
+                this.services.push(this.unService);
+                this.serviceSubject.next(this.services);
                     this.messageService.add({ severity: 'success', summary: 'Success', detail: response.message, life: 3000 });
                 }
             },
